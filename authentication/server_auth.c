2c02ff82b60425a7c3a46867a392d3aef29b9760
nicolamaritan <nicolamaritan01@gmail.com>
2023-05-06 16:45:44 +0200

    Added base64 decoding to authorization

--- authentication/server_auth.c
+++ authentication/server_auth.c
@@ -6,18 +6,38 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
+
 
 #define BACKLOG 10
 #define MAX_H 100
 #define MAX_HBUF 5000
 #define MAX_RL 100
 #define MAX_RES 1000
+#define MAX_DECODE 1000
 
 struct header
 {
    char* n;
    char* v;
 } h[MAX_H];
+
+static char encoding_table[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                                'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                                'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+                                'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+                                'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                                'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                                'w', 'x', 'y', 'z', '0', '1', '2', '3',
+                                '4', '5', '6', '7', '8', '9', '+', '/'};
+static char *decoding_table = NULL;
+static int mod_table[] = {0, 2, 1};
+void build_decoding_table(); 
+void base64_cleanup();
+unsigned char *base64_decode(const char *data,
+                             size_t input_length,
+                             size_t *output_length);
+
 
 int main()
 {
@@ -81,6 +101,7 @@ int main()
                break;
             }
             h[++j].n = hbuf+i+1;
+            h[j].v = 0; // Override previous connection value
          }
          else if (hbuf[i] == ':' && !h[j].v )
          {
@@ -98,6 +119,26 @@ int main()
       for (i=0; h[i].n[0]; i++)
       {
          printf("%s : %s\n", h[i].n, h[i].v);
+         
+         if (strcmp(h[i].n, "Authorization") == 0)
+         {
+            printf("OOOOOOOOOOOOOOOOOOOOOOOOOO");
+            char* encoded = h[i].v + 7;   // skip Basic
+            size_t length = 0;
+            char* out = base64_decode(encoded, strlen(encoded), &length);
+            out[length] = 0;
+
+            char* out_user = out;
+            for (j=0; out[j]!=':'; j++);  // Parse decoded text
+            out[j] = 0;
+            char* out_psw = out+j+1;
+
+            // Checks for correct user and password
+            if (strcmp(user, out_user) == 0 && strcmp(psw, out_psw) == 0)
+            {
+               authorized = 1;
+            }
+         }
       }
 
       char response[MAX_RES];
@@ -115,4 +156,53 @@ int main()
       printf("*** Closing connection.\n\n");
       close(c);
    }
+}
+
+// Code from https://stackoverflow.com/questions/342409/how-do-i-base64-encode-decode-in-c
+void build_decoding_table() {
+
+    decoding_table = malloc(256);
+
+    for (int i = 0; i < 64; i++)
+        decoding_table[(unsigned char) encoding_table[i]] = i;
+}
+
+
+void base64_cleanup() {
+    free(decoding_table);
+}
+
+unsigned char *base64_decode(const char *data,
+                             size_t input_length,
+                             size_t *output_length) {
+
+    if (decoding_table == NULL) build_decoding_table();
+
+    if (input_length % 4 != 0) return NULL;
+
+    *output_length = input_length / 4 * 3;
+    if (data[input_length - 1] == '=') (*output_length)--;
+    if (data[input_length - 2] == '=') (*output_length)--;
+
+    unsigned char *decoded_data = malloc(*output_length);
+    if (decoded_data == NULL) return NULL;
+
+    for (int i = 0, j = 0; i < input_length;) {
+
+        uint32_t sextet_a = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+        uint32_t sextet_b = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+        uint32_t sextet_c = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+        uint32_t sextet_d = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+
+        uint32_t triple = (sextet_a << 3 * 6)
+        + (sextet_b << 2 * 6)
+        + (sextet_c << 1 * 6)
+        + (sextet_d << 0 * 6);
+
+        if (j < *output_length) decoded_data[j++] = (triple >> 2 * 8) & 0xFF;
+        if (j < *output_length) decoded_data[j++] = (triple >> 1 * 8) & 0xFF;
+        if (j < *output_length) decoded_data[j++] = (triple >> 0 * 8) & 0xFF;
+    }
+
+    return decoded_data;
 }
