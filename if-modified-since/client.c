c387f12bcd0df63af2d7d23d51cdb9edf705a608
Nicola Maritan <nicolamaritan01@gmail.com>
2023-05-28 09:28:08 +0200

    if-modified-since update

--- if-modified-since/client.c
+++ if-modified-since/client.c
@@ -6,92 +6,121 @@
 #include <errno.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define MAX_H 100
 #define MAX_HBUF 5000
 #define MAX_RL 100
+#define MAX_BODY 1000
 
 char hbuf[MAX_HBUF+1];
 char rl[MAX_RL+1];
 struct header
 {
-   char* n;
-   char* v;
+    char* n;
+    char* v;
 }h[MAX_H];
 
 int main()
 {
-   int s;
-   s = socket(AF_INET, SOCK_STREAM, 0);
+    int s;
+    s = socket(AF_INET, SOCK_STREAM, 0);
 
-   if (s<0)
-   {
-      perror("Socket creation failed.\n");
-      printf("%d\n", errno);
-      return 1;
-   }
+    if (s<0)
+    {
+        perror("Socket creation failed.\n");
+        printf("%d\n", errno);
+        return 1;
+    }
 
-   struct sockaddr_in add;
-   add.sin_family = AF_INET;
-   add.sin_port = htons(8081);
-   char* ptr = (char*)&add.sin_addr.s_addr;
-   ptr[0]=127;
-   ptr[1]=0;
-   ptr[2]=0;
-   ptr[3]=1;
+    struct sockaddr_in add;
+    add.sin_family = AF_INET;
+    add.sin_port = htons(8081);
+    char* ptr = (char*)&add.sin_addr.s_addr;
+    ptr[0]=127;
+    ptr[1]=0;
+    ptr[2]=0;
+    ptr[3]=1;
 
-   if (connect(s, (struct sockaddr*)&add, sizeof(add)) < 0)
-   {
-      perror("Socket connection failed.\n");
-      printf("%d\n", errno);
-      return 1;
-   }
+    if (connect(s, (struct sockaddr*)&add, sizeof(add)) < 0)
+    {
+        perror("Socket connection failed.\n");
+        printf("%d\n", errno);
+        return 1;
+    }
 
-   char* request = "GET / HTTP/1.1\r\nIf-Modified-Since: Sat, 29 Oct 1994 19:43:31 GMT\r\n\r\n";
+    // Testing with these two request - date represents the last resource "update"
+    char* request = "GET / HTTP/1.1\r\nIf-Modified-Since:Sat, 28 May 2023 20:00:00 GMT\r\n\r\n";
+    //char* request = "GET / HTTP/1.1\r\nIf-Modified-Since:Sat, 29 Oct 1994 19:00:00 GMT\r\n\r\n";
 
-   write(s, request, strlen(request));
+    write(s, request, strlen(request));
 
-   // Request-Line parse   
-   int i;
-   for(i=0; i<MAX_RL && read(s, rl+i, 1); i++)
-   {
-      if (rl[i]=='\n' && rl[i-1] == '\r')
-      {
-         rl[i-1] = 0;
-         break;
-      }
-   }
-
-   // Headers parse
-   int j;
-   h[0].n = hbuf;
-   for (i=0, j=0; i<MAX_HBUF && j<MAX_H && read(s, hbuf+i, 1); i++)
-   {
-      if (hbuf[i]=='\n' && hbuf[i-1] == '\r')
-      {
-         hbuf[i-1]=0;
-         if (h[j].n[0] == 0)
-         {
+    // Request-Line parse   
+    int i;
+    for(i=0; i<MAX_RL && read(s, rl+i, 1); i++)
+    {
+        if (rl[i]=='\n' && rl[i-1] == '\r')
+        {
+            rl[i-1] = 0;
             break;
-         }
-         h[++j].n = hbuf+i+1;
-      }
-      else if (hbuf[i] == ':' && !h[j].v)
-      {
-         hbuf[i] = 0;
-         h[j].v = hbuf+i+1;
-      }
-   }
+        }
+    }
 
-   printf("Status-Line: %s\n", rl);
+    // Headers parse
+    int j;
+    h[0].n = hbuf;
+    for (i=0, j=0; i<MAX_HBUF && j<MAX_H && read(s, hbuf+i, 1); i++)
+    {
+        if (hbuf[i]=='\n' && hbuf[i-1] == '\r')
+        {
+            hbuf[i-1]=0;
+            if (h[j].n[0] == 0)
+            {
+                break;
+            }
+            h[++j].n = hbuf+i+1;
+        }
+        else if (hbuf[i] == ':' && !h[j].v)
+        {
+            hbuf[i] = 0;
+            h[j].v = hbuf+i+1;
+        }
+    }
 
-   for (j=0; h[j].n[0]; j++)
-   {
-      printf("%s: %s\n", h[j].n, h[j].v);
-   }
+    printf("Status-Line: %s\n", rl);
+    
+    // Parse method
+    for (i=0; i<strlen(rl); i++)
+    {
+        if (rl[i] == ' ')
+        {
+            rl[i] = 0;
+        }
+    }
+
+    int code = atoi(rl+9);
+
+    if (code == 304)
+    {
+        // Cached value
+        printf("Server message: Hello World!\n");
+        return 0;
+    }
+
+    int body_length = -1;
 
 
+    for (j=0; h[j].n[0]; j++)
+    {
+        printf("%s: %s\n", h[j].n, h[j].v);
+        if (strcmp("Content-Length", h[j].n) == 0)
+        {
+            body_length = atoi(h[j].v);
+        }
+    }
 
-
-
+    char body[MAX_BODY+1];
+    for (i=0; i<body_length && i<MAX_BODY && read(s, body+i,1); i++)
+    
+    printf("Server message: %s", body);
 }
--- if-modified-since/server.c
+++ if-modified-since/server.c
@@ -13,134 +13,154 @@
 #include <assert.h>
 #define MAX_RES 1000
 
+int date_compare(const char* date1, const char* date2)
+{
+    struct tm tm1, tm2;
+    time_t time1, time2;
+
+    // Convert the first HTTP-Date string to time_t
+    strptime(date1, "%a, %d %b %Y %H:%M:%S %Z", &tm1);
+    time1 = mktime(&tm1);
+
+    // Convert the second HTTP-Date string to time_t
+    strptime(date2, "%a, %d %b %Y %H:%M:%S %Z", &tm2);
+    time2 = mktime(&tm2);
+
+    // Compare the time_t values
+    if (time1 < time2)
+        return -1;
+    else if (time1 > time2)
+        return 1;
+    else
+        return 0;
+}
+
 int main()
 {
-   int s;
-   s = socket(AF_INET, SOCK_STREAM, 0);
-   if (s < 0)
-   {
-      perror("Socket creation failed\n");
-      printf("%d\n", errno);
-      return 1;
-   }
-   int opt = 1;
-   if (setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(int)) < 0)
-   {
-      perror("Socket options failed\n");
-      printf("%d\n", errno);
-      return 1;
-   }
+    int s;
+    s = socket(AF_INET, SOCK_STREAM, 0);
+    if (s < 0)
+    {
+        perror("Socket creation failed\n");
+        printf("%d\n", errno);
+        return 1;
+    }
+    int opt = 1;
+    if (setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(int)) < 0)
+    {
+        perror("Socket options failed\n");
+        printf("%d\n", errno);
+        return 1;
+    }
 
-   struct sockaddr_in addr;
-   addr.sin_family = AF_INET;
-   addr.sin_port = htons(8081);
-   if (bind(s, (struct sockaddr*)&addr, sizeof(addr)) < 0)
-   {
-      perror("Socket binding failed\n");
-      printf("%d\n", errno);
-      return 1;
-   }
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(8081);
+    if (bind(s, (struct sockaddr*)&addr, sizeof(addr)) < 0)
+    {
+        perror("Socket binding failed\n");
+        printf("%d\n", errno);
+        return 1;
+    }
 
-   const int BACKLOG = 10;
-   if (listen(s, BACKLOG) < 0)
+    const int BACKLOG = 10;
+    if (listen(s, BACKLOG) < 0)
 
-   {
-      perror("Socket listen failed\n");
-      printf("%d\n", errno);
-      return 1;
-   }
+    {
+        perror("Socket listen failed\n");
+        printf("%d\n", errno);
+        return 1;
+    }
 
-   while (1)
-   {
-      struct sockaddr_in remote_addr;
-      int size = sizeof(remote_addr);
-      int c = accept(s, (struct sockaddr*)&remote_addr, &size);
+    while (1)
+    {
+        struct sockaddr_in remote_addr;
+        int size = sizeof(remote_addr);
+        int c = accept(s, (struct sockaddr*)&remote_addr, &size);
 
-      if (c == -1)
-      {
-         perror("Remote connection failed\n");
-         printf("%d\n", errno);
-         continue;
-      }
+        if (c == -1)
+        {
+            perror("Remote connection failed\n");
+            printf("%d\n", errno);
+            continue;
+        }
 
-      printf("Connection accepted.\n");
+        printf("Connection accepted.\n");
 
-      const int RL_SIZE = 100;
-      const int HBUF_SIZE = 10000;
-      char rl[RL_SIZE];
-      char hbuf[HBUF_SIZE];
+        const int RL_SIZE = 100;
+        const int HBUF_SIZE = 10000;
+        char rl[RL_SIZE];
+        char hbuf[HBUF_SIZE];
 
-      int i;
+        int i;
 
-      // Response line parsing
-      for (i=0; i<RL_SIZE && read(c, rl+i, 1) && rl[i] != '\n' && rl[i-1] != '\r'; i++);
-      rl[i] = 0;
+        // Response line parsing
+        for (i=0; i<RL_SIZE && read(c, rl+i, 1) && rl[i] != '\n' && rl[i-1] != '\r'; i++);
+        rl[i] = 0;
 
-      const int H_SIZE = 100;
-      int j;
-      struct header
-      {
-         char* n;
-         char* v;
-      } h[H_SIZE];
+        const int H_SIZE = 100;
+        int j;
+        struct header
+        {
+            char* n;
+            char* v;
+        } h[H_SIZE];
 
-      h[0].n = hbuf;
-      // Header parsing
-      for (i=0, j=0; i<HBUF_SIZE && j<H_SIZE && read(c, hbuf+i, 1); i++)
-      {
-         if (hbuf[i] == '\n' && hbuf[i-1] == '\r')
-         {
-            hbuf[i-1] = 0;
-            if (h[j].n[0] == 0)
+        h[0].n = hbuf;
+        h[0].v = 0;
+        // Header parsing
+        for (i=0, j=0; i<HBUF_SIZE && j<H_SIZE && read(c, hbuf+i, 1); i++)
+        {
+            if (hbuf[i] == '\n' && hbuf[i-1] == '\r')
             {
-               break;
+                hbuf[i-1] = 0;
+                if (h[j].n[0] == 0)
+                {
+                    break;
+                }
+                h[++j].n = hbuf+i+1;  
+                h[j].v = 0;
             }
-            h[++j].n = hbuf+i+1;      
-         }
-         else if (hbuf[i] == ':' && !h[j].v)
-         {
-            hbuf[i] = 0;
-            h[j].v = hbuf+i+1;
-         }
-      }
-
-      char response[MAX_RES+1];
-      struct tm ims;
-      struct tm lm;
-      strptime("Wed, 28 Apr 2023 20:31:05 GMT", "%a, %d %b %Y %H:%M:%S %Z", &lm);
-
-
-      // Print request line and headers
-      printf("%s\n", rl);
-      for (j=0; h[j].n[0]; j++)
-      {
-         printf("%s : %s\n", h[j].n, h[j].v);
-         if (strcmp(h[j].n, "If-Modified-Since") == 0)
-         {
-
-            strptime(h[j].v, "%a, %d %b %Y %H:%M:%S %Z", &ims);
-            double diff = difftime(mktime(&ims), mktime(&lm));
-
-            if (diff < 0)
+            else if (hbuf[i] == ':' && !h[j].v)
             {
-               sprintf(response, "HTTP/1.1 304 Not Modified\r\n\r\n");
+                hbuf[i] = 0;
+                h[j].v = hbuf+i+1;
             }
-            else
+        }
+
+        char response[MAX_RES+1];
+
+        // Print request line and headers
+        printf("%s\n", rl);
+        for (j=0; h[j].n[0]; j++)
+        {
+            printf("%s : %s\n", h[j].n, h[j].v);
+            if (strcmp(h[j].n, "If-Modified-Since") == 0)
             {
-               sprintf(response, "HTTP/1.1 200 OK\r\n\r\n");
+                char* last_modified = "Wed, 28 Apr 2023 20:31:05 GMT";
+                int diff = date_compare(last_modified, h[j].v);
+
+                if (diff > 0)
+                {
+                    sprintf(response, "HTTP/1.1 200 OK\r\nContent-Lenght:12\r\nHello World!");
+                }
+                else
+                {
+
+                    sprintf(response, "HTTP/1.1 304 Not Modified\r\n\r\n");
+                }
             }
-         }
-      }
+        }
 
-      while (send(c, response, strlen(response), 0) < 0)
-      {
-         perror("Error writing\n");
-         printf("%d\n", errno);
-      }
+        while (send(c, response, strlen(response), 0) < 0)
+        {
+            perror("Error writing\n");
+            printf("%d\n", errno);
+        }
 
-      printf("End of connection\n\n");
-      close(c);
-   }
+        printf("End of connection\n**********************************\n\n");
+        close(c);
+    }
 }
 
 
