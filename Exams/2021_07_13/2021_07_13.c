ac6f930b4a3d65d80351e6032227b3d429f1acc1
nicolamaritan <nicolamaritan01@gmail.com>
2023-06-23 18:28:57 +0200

    added 2021 exam

--- Exams/2021_07_13/2021_07_13.c
+++ Exams/2021_07_13/2021_07_13.c
@@ -1,3 +1,5 @@
+// Thanks to Davide Molinaroli
+
 #include<stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,8 +13,8 @@
 #include <netinet/in.h>
 #include <sys/types.h>
 #include <signal.h>
-#include <assert.h>
 
+#define LEN 10000
 
 int pid;
 struct sockaddr_in local, remote, server;
@@ -27,6 +29,9 @@ struct header {
 } h[100];
 
 struct hostent * he;
+
+char entity[2000000];
+int firstByte;
 
 int main()
 {
@@ -104,84 +109,40 @@ int main()
 
 			he = gethostbyname(hostname);
 			printf("%d.%d.%d.%d\n",(unsigned char) he->h_addr[0],(unsigned char) he->h_addr[1],(unsigned char) he->h_addr[2],(unsigned char) he->h_addr[3]); 
-			if (( s3 = socket(AF_INET, SOCK_STREAM, 0 )) == -1)
-			{ printf("errno = %d\n",errno); perror("Socket Fallita"); exit(-1); }
-
-			server.sin_family = AF_INET;
-			server.sin_port =htons(80);
-			server.sin_addr.s_addr = *(unsigned int *)(he->h_addr);
-
-			if(-1 == connect(s3,(struct sockaddr *) &server, sizeof(struct sockaddr_in)))
-			{perror("Connect Fallita"); exit(1);}	
-
-			#define SIZE 100000
-			int firstBytePos = 0;
-			int lastBytePos = SIZE-1;
-			char piece[SIZE];
-			int p=0;
 
 
-			int toExit = 0;
+			char filename2[100];
+			char hostname2[100];
+			char contType[100];
 
-			char filenameStr[100];
-			char hostnameStr[100];
-
-			strcpy(filenameStr, filename);
-			strcpy(hostnameStr, hostname);
+			strcpy(filename2, filename);
+			strcpy(hostname2, hostname);
 
 
-			sprintf(request,"HEAD /%s HTTP/1.1\r\nHost:%s\r\n\r\n",filenameStr,hostnameStr);
-			write(s3,request,strlen(request));
-
-			printf("**************server header parsing\n");
-			// Parsing headers
-			bzero(hbuffer,10000);
-			bzero(h,100*sizeof(struct header));
-			reqline = h[0].n = hbuffer;
-			for (i=0,j=0; read(s3,hbuffer+i,1); i++) {
-				printf("%c",hbuffer[i]);
-				if(hbuffer[i]=='\n' && hbuffer[i-1]=='\r'){
-					hbuffer[i-1]=0; // Termino il token attuale
-					if (!h[j].n[0]) break;
-					h[++j].n=hbuffer+i+1;
-				}
-				if (hbuffer[i]==':' && !h[j].v && j>0){
-					hbuffer[i]=0;
-					h[j].v = hbuffer + i + 1;
-				}
-			}
-
-			int fullCl;
-			for (i=0; h[i].n[0]; i++)
+			firstByte = 0;
+			int len=0;
+			for (int b=0; 1; b++)
 			{
-				//printf("%s:%s\n", h[i].n, h[i].v);
-				if (strcmp("Content-Length", h[i].n) == 0)
-				{
-					fullCl = atoi(h[i].v+1);
-				}
-			}
+				if (( s3 = socket(AF_INET, SOCK_STREAM, 0 )) == -1)
+				{ printf("errno = %d\n",errno); perror("Socket Fallita"); exit(-1); }
 
-			printf("fullImageLen: %d\n", fullCl);
-			sprintf(response, "HTTP/1.1 200 OK\r\nContent-Type:image/jpeg\r\nContent-Length:%d\r\n\r\n", fullCl);
-			write(s2, response, strlen(response));
+				server.sin_family = AF_INET;
+				server.sin_port =htons(80);
+				server.sin_addr.s_addr = *(unsigned int *)(he->h_addr);
 
+				if(-1 == connect(s3,(struct sockaddr *) &server, sizeof(struct sockaddr_in)))
+				{perror("Connect Fallita"); exit(1);}	
 
-			int cl;
-			toExit = 1;
-			while (1)
-			{
-				sprintf(request,"GET /%s HTTP/1.1\r\nHost:%s\r\nRange:bytes=%d-%d\r\n\r\n",filenameStr,hostnameStr, firstBytePos, lastBytePos);
-
-				printf("%s, from socket %d\n", request, s2);
+				sprintf(request,"GET /%s HTTP/1.1\r\nHost:%s\r\nConnection:close\r\nRange:bytes=%d-%d\r\n\r\n",filename2,hostname2, firstByte, firstByte+LEN-1);
+				printf("%s\n",request);
 				write(s3,request,strlen(request));
 
-				printf("**************server header parsing\n");
-				// Parsing headers
+				// Header parsing
 				bzero(hbuffer,10000);
 				bzero(h,100*sizeof(struct header));
 				reqline = h[0].n = hbuffer;
 				for (i=0,j=0; read(s3,hbuffer+i,1); i++) {
-					printf("%c",hbuffer[i]);
+					//printf("%c",hbuffer[i]);
 					if(hbuffer[i]=='\n' && hbuffer[i-1]=='\r'){
 						hbuffer[i-1]=0; // Termino il token attuale
 						if (!h[j].n[0]) break;
@@ -193,36 +154,41 @@ int main()
 					}
 				}
 
-
-				int cl;
-				toExit = 1;
 				for (i=0; h[i].n[0]; i++)
 				{
-					//printf("%s:%s\n", h[i].n, h[i].v);
-					if (strcmp("Content-Length", h[i].n) == 0)
+					if (b==0)
 					{
-						cl = atoi(h[i].v+1);
-					}
-					if (strcmp("Content-Range", h[i].n) == 0)
-					{
-						toExit = 0;
+						if (strcmp(h[i].n, "Content-Range") == 0)
+						{
+							len = atoi(strstr(h[i].v, "/")+1);
+							printf("len: %d\n", len);
+						}
+						else if (strcmp(h[i].n, "Content-Type") == 0)
+						{
+							strcpy(contType, h[i].v+1);
+							printf("contType: %s\n", contType);
+						}
+
 					}
 				}
 
-				for(i=0;i<cl && (t=read(s3,piece+i,SIZE-i));i+=t);
-				for(i=0;i<cl && (t=write(s2,piece+i,cl-i));i+=t);
 
-				printf("read bytes=%d-%d\n\n", firstBytePos, firstBytePos+cl);
-				firstBytePos += SIZE;
-				lastBytePos += SIZE;
+				/*while ( t=read(s3,buffer,2000))
+				  write(s2,buffer,t);*/
 
-				if (toExit)
-					break;	
-				p++;
+				for (t=0, i=0; (t=read(s3, entity+i+firstByte, LEN-i)); i+=t);
+
+				close(s3);
+
+				firstByte += LEN;
+				if (firstByte >= len)
+					break;
 			}
-			printf("closing s3 and s2, p=%d\n", p);
+
+			sprintf(response, "HTTP/1.1 200 OK\r\nContent-Length:%d\r\nConnection:close\r\nContent-Type:%s\r\n\r\n", len, contType);
+			write(s2, response, strlen(response));
+			for (t=0, i=0; i<len && (t=write(s2, entity+i, len-i)); i+=t);
 			close(s2);
-			close(s3);
 
 
 		}
